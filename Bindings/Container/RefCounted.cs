bd0247ea892e3a04f026b25da3a0803621859104
1vanK <1vanK@users.noreply.github.com>
2017-11-12 16:07:08 +0300

    Update

--- Bindings/Container/RefCounted.cs
+++ Bindings/Container/RefCounted.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Runtime.InteropServices;
 
-abstract public class RefCounted
+public abstract class RefCounted
 {
     internal IntPtr NativeInstance { get; private set; }
     
